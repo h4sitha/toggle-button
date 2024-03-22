0995b3c50b7f48b2978d4b97cebf7c62ad6c3129
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-22 21:55:02 +0530

    Update function to change background color

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const toggleBtn = document.querySelector('input[type="checkbox"]');
 
 toggleBtn.addEventListener('change', (e) => {
     if (e.target.checked) {
-        console.log("Checked!");
+        document.body.style.backgroundColor = "hsl(120, 73%, 90%)";
     } else {
-        console.log("Not Checked!");
+        document.body.style.backgroundColor = "hsl(0, 0%, 95%)";
     }
 })
